a6be027de33ba368bfccd83d31c11da80cacebd4
Shaul Carvalho <shaulc@comm-it.com>
2025-05-06 11:04:14 +0300

    remove verything that's not helm

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM python:3.12.9-alpine
-COPY src/* .
-COPY requirements.txt .
-RUN pip install -r requirements.txt
-EXPOSE 8080
-CMD ["python", "app.py"]
--- src/app.py
+++
@@ -1,6 +0,0 @@
-from nicegui import ui
-
-with ui.column().classes('items-center w-full'):
-    ui.label('Task Complete').classes('text-red text-7xl')
-
-ui.run(reload=False)
